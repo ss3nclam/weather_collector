51abbc52768cdc6dd1c9e139dbcf2171ad98391b
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2023-11-14 04:52:37 +0400

    updated

--- app.py
+++ app.py
@@ -3,31 +3,36 @@ import logging
 
 import schedule
 
-from app_core.data import (check_cities_table, commit_weather, get_cities_list,
-                           get_weather, init_cities, validate_db)
+from app_core.data import (
+    check_cities_table,
+    commit_weather,
+    get_cities_list,
+    get_weather,
+    init_cities,
+    validate_db,
+)
 from app_core.settings import app_verbose, config
 
 
 def job(cities):
     for city in cities:
-        weather = get_weather(city['name'], city['longitude'], city['latitude'])
+        weather = get_weather(city["name"], city["longitude"], city["latitude"])
         if weather:
-            commit_weather(city['name'], weather)
+            commit_weather(city["name"], weather)
 
 
 def main():
     validate_db()
     cities_list = get_cities_list()
     if cities_list:
-        time_delta = int(config['APP']['scheduler_time_delta'])
+        time_delta = int(config["APP"]["scheduler_time_delta"])
         schedule.every(time_delta).hours.do(job, cities_list)
-        # schedule.every(15).seconds.do(job, cities_list)
         while True:
             schedule.run_pending()
             time.sleep(1)
     else:
-        logging.error('Something was wrong!')
+        logging.error("Something was wrong!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
